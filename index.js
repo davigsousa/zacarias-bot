ef7424a57b448312335b9da29b424e1f64b738f3
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-06-24 10:28:33 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+const Discord = require("discord.js");
+const axios = require("axios");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content === "ping") {
+    message.channel.send("pong");
+  }
+
+  if (
+    message.content.toLowerCase().includes("otario") ||
+    message.content.toLowerCase().includes("otaru") ||
+    message.content.toLowerCase().includes("otário")
+  ) {
+    message.channel.send("Euuuuu!!");
+  }
+
+  if (message.content.toLowerCase().includes("piada")) {
+    (async () => {
+      const response = await axios.default.get(
+        "https://us-central1-kivson.cloudfunctions.net/charada-aleatoria",
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      const { pergunta, resposta } = response.data;
+
+      message.channel.send(pergunta);
+      setTimeout(() => message.channel.send(resposta), 1000);
+    })();
+  }
+});
+
+client.login("NzI1MTY5NTg1NTEwMTU0Mjgz.XvK26Q.s0dUM0A2xDOzXuE7VyDIgP64xqk");
