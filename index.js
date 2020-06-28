0f9f1e9d0581a6aeed189f7b5a9990c6823c358f
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-06-27 23:05:33 -0300

    =saves refactored

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const axios = require("axios");
 const client = new Discord.Client();
 
 let first = true;
-let update = new Date();
-let updateDate = `${String(update.getDate()).padStart(2, "0")}/${String(
+const update = new Date();
+const updateDate = `${String(update.getDate()).padStart(2, "0")}/${String(
   update.getMonth() + 1
 ).padStart(2, "0")}/${update.getFullYear()}`;
+const updateMessage = `Última atualização realizada em ${updateDate}. Os saves antes disso foram perdidos.`;
 const saves = {};
 
 client.on("ready", () => {
@@ -17,9 +18,7 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (first) {
-    message.channel.send(
-      `Última atualização realizada em ${updateDate}. Os saves antes disso foram perdidos.`
-    );
+    message.channel.send(updateMessage);
     first = false;
   }
 
@@ -61,22 +60,31 @@ client.on("message", (message) => {
   }
 
   // Comandos
-  if (starts("=save")) {
+  if (starts("=saves")) {
     const parts = message.content.split(" ");
-    saves[parts[1]] = parts[2];
-    message.channel.send(`${parts[1]} salvo com sucesso!`);
-  }
 
-  if (starts("=get")) {
-    const parts = message.content.split(" ");
-    if (saves.hasOwnProperty(parts[1])) {
-      message.channel.send(saves[parts[1]]);
-    } else {
-      message.channel.send("Infelizmente não pude achar essa informação.");
+    if (contain("add")) {
+      saves[parts[2]] = parts[3];
+      message.channel.send(`${parts[3]} salvo com sucesso!`);
+    }
+
+    if (contain("get")) {
+      if (saves.hasOwnProperty(parts[2])) {
+        message.channel.send(saves[parts[2]]);
+      } else {
+        message.channel.send("Infelizmente não pude achar essa informação.");
+      }
+    }
+
+    if (contain("list")) {
+      message.channel.send("Informações salvas:");
+      for (key in saves.keys()) {
+        message.channel.send(key);
+      }
     }
   }
 
-  if (starts("=update")) message.channel.send(updateDate);
+  if (starts("=update")) message.channel.send(updateMessage);
 });
 
 client.login("NzI1MTY5NTg1NTEwMTU0Mjgz.XvK26Q.s0dUM0A2xDOzXuE7VyDIgP64xqk");
