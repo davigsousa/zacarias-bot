0850fe93ed6bf34a5f23dfcee7023f88663a7a7a
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-01-17 18:28:14 -0300

    help dialog improved

--- src/resources/dialogs.ts
+++ src/resources/dialogs.ts
@@ -1,4 +1,4 @@
 export default {
   help:
-    "Comandos:\n -z audios | Exibe os áudios predefinidos.\nPara ouvir um áudio predefinido, basta enviar: -z nome do audio, exemplo, -z boa noite.\nUse o comando random, para ouvir uma áudio aleatório, ex: -z random.\n -z leave | Desconecta o bot de um canal de áudio.\n -z piada ou -z charada || Envia um piada ou charada aleatória.",
+    "**Comandos:** \n *-z audios* | Exibe os áudios predefinidos.\nPara ouvir um áudio predefinido, basta enviar: *-z nome do audio*, exemplo, *-z boa noite*.\n*-z random* | Ouve áudio aleatório da internet.\n *-z leave* | Desconecta o bot de um canal de áudio.\n *-z piada* ou *-z charada* | Envia um piada ou charada aleatória.",
 };
